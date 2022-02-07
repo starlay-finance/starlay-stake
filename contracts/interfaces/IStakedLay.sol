41c5a37ac28975c97e0daf3ad593c12634261a29
linnefromice <linnefromice@gmail.com>
2022-02-07 21:57:26 +0900

    rename - IStakedAave -> IStakedLay

--- contracts/interfaces/IStakedLay.sol
+++ contracts/interfaces/IStakedLay.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.7.5;
 
-interface IStakedAave {
+interface IStakedLay {
   function stake(address to, uint256 amount) external;
 
   function redeem(address to, uint256 amount) external;
