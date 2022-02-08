e9149861db89a8c0c11892bf50d95f5757d02c24
linnefromice <linnefromice@gmail.com>
2022-02-09 00:29:05 +0900

    use StarlayProtocolDataProvider in starlay-protocol instead of @aave/protocol-v2

--- contracts/proposals/extend-stkaave-distribution/interfaces/LendingPoolInterfaces.sol
+++ contracts/proposals/extend-stkaave-distribution/interfaces/LendingPoolInterfaces.sol
@@ -1,5 +1,4 @@
 import '../../../../submodule-protocol/contracts/interfaces/ILendingPool.sol';
 import '../../../../submodule-protocol/contracts/interfaces/ILendingPoolAddressesProvider.sol';
 import '../../../../submodule-protocol/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
-import '@aave/protocol-v2/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
-import '@aave/protocol-v2/contracts/misc/AaveProtocolDataProvider.sol';
+import '../../../../submodule-protocol/contracts/misc/StarlayProtocolDataProvider.sol';
