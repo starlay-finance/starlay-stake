140cb0005f35c9a87395b90158e1892bc5ebbe63
linnefromice <linnefromice@gmail.com>
2022-02-23 12:49:42 +0900

    set parameters for astar (for test)

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -55,7 +55,7 @@ export const getTokenPerNetwork = (network: eEthereumNetwork | eAstarNetwork): t
       [eEthereumNetwork.rinkeby]: ZERO_ADDRESS,
       [eEthereumNetwork.kovan]: '0x75AC15EbCA4e93D61bCc878ded9Ba338FD23E761', // Mock Lay Token
       [eEthereumNetwork.main]: '0x9c0435779F5E52CEC404D957C9bAa6f7d674C8bA',
-      [eAstarNetwork.astar]: ZERO_ADDRESS, // Dummy
+      [eAstarNetwork.astar]: '0x68692054974A8026A9838C338ea53F79539c79af', // Dummy
       [eAstarNetwork.shiden]: '0xb163716cb6c8b0a56e4f57c394A50F173E34181b',
       [eAstarNetwork.shibuya]: '0xEB741C051E474f83cff38B44A912281772C23CE6'
     },
@@ -101,7 +101,7 @@ export const getAdminPerNetwork = (network: eEthereumNetwork | eAstarNetwork): t
       [eEthereumNetwork.rinkeby]: ZERO_ADDRESS,
       [eEthereumNetwork.kovan]: '0x175d905470e85279899C37F89000b195f3d0c0C5', // '0x8134929c3dcb1b8b82f27f53424b959fb82182f2', // Governance
       [eEthereumNetwork.main]: '0x8a2Efd9A790199F4c94c6effE210fce0B4724f52', // Governance
-      [eAstarNetwork.astar]: ZERO_ADDRESS, // Dummy
+      [eAstarNetwork.astar]: '0x175d905470e85279899C37F89000b195f3d0c0C5', // Dummy
       [eAstarNetwork.shiden]: '0x175d905470e85279899C37F89000b195f3d0c0C5', // Dummy
       [eAstarNetwork.shibuya]: '0x175d905470e85279899C37F89000b195f3d0c0C5'
     },
@@ -135,7 +135,7 @@ export const getIncentivesVaultPerNetwork = (network: eEthereumNetwork | eAstarN
       [eEthereumNetwork.rinkeby]: ZERO_ADDRESS,
       [eEthereumNetwork.kovan]: '0x175d905470e85279899C37F89000b195f3d0c0C5',
       [eEthereumNetwork.main]: '0x253f7b06c1d60c1fbbc9d82c301327eb86e3ba81',
-      [eAstarNetwork.astar]: ZERO_ADDRESS, // Dummy
+      [eAstarNetwork.astar]: '0xE762Cf0990D1e35fA95A9E99093aB5203e8dD573', // Dummy
       [eAstarNetwork.shiden]: '0x175d905470e85279899C37F89000b195f3d0c0C5', // Dummy
       [eAstarNetwork.shibuya]: '0x175d905470e85279899C37F89000b195f3d0c0C5' // Dummy
     },
