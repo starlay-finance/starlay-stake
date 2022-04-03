d7b6e8e0b7101036de39acb96b121d4c1bd494e9
atatur9 <momodarou2015@gmail.com>
2022-04-03 19:51:44 +0900

    fix: SKP-01 | Lack Of Zero Address Validation

--- test/IncentivesController/constructor.spec.ts
+++ test/IncentivesController/constructor.spec.ts
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 
 import { makeSuite } from '../helpers/make-suite';
 import { deployIncentivesController } from '../../helpers/contracts-accessors';
-import { RANDOM_ADDRESSES } from '../../helpers/constants';
+import { RANDOM_ADDRESSES, ZERO_ADDRESS } from '../../helpers/constants';
 
 makeSuite('IncentivesController constructor tests', () => {
   it('should assign correct params', async () => {
@@ -35,5 +35,53 @@ makeSuite('IncentivesController constructor tests', () => {
     await expect((await incentivesController.DISTRIBUTION_END()).toString()).to.be.equal(
       (await timeLatest()).plus(distributionDuration).toString()
     );
+  });
+  it('constructor reverts if initializes with zero_address', async () => {
+    const peiEmissionManager = RANDOM_ADDRESSES[2];
+    const rewardToken = RANDOM_ADDRESSES[3];
+    const rewardsVault = RANDOM_ADDRESSES[4];
+    const psm = RANDOM_ADDRESSES[5];
+    const extraPsmReward = '100';
+    const distributionDuration = '100';
+    await expect(
+      deployIncentivesController([
+        ZERO_ADDRESS,
+        rewardsVault,
+        psm,
+        extraPsmReward,
+        peiEmissionManager,
+        distributionDuration,
+      ])
+    ).to.be.revertedWith('Cannot set the rewardToken to the zero address');
+    await expect(
+      deployIncentivesController([
+        rewardToken,
+        ZERO_ADDRESS,
+        psm,
+        extraPsmReward,
+        peiEmissionManager,
+        distributionDuration,
+      ])
+    ).to.be.revertedWith('Cannot set the rewardsVault to the zero address');
+    await expect(
+      deployIncentivesController([
+        rewardToken,
+        rewardsVault,
+        ZERO_ADDRESS,
+        extraPsmReward,
+        peiEmissionManager,
+        distributionDuration,
+      ])
+    ).to.be.revertedWith('Cannot set the psm to the zero address');
+    await expect(
+      deployIncentivesController([
+        rewardToken,
+        rewardsVault,
+        psm,
+        extraPsmReward,
+        ZERO_ADDRESS,
+        distributionDuration,
+      ])
+    ).to.be.revertedWith('Cannot set the emissionManager to the zero address');
   });
 });
